77b54caa2af0f44afdad9981d378e46cdff0369e
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-10-01 17:42:26 +0100

    minor change

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,10 +1,12 @@
 from .circuit_breaker import CircuitBreaker, CircuitBreakerError
 from .currency_manager import CurrencyManager
 from .rate_aggregator import RateAggregatorService
+from .service_factory import ServiceFactory
 
 __all__ = [
     "CircuitBreaker",
     "CircuitBreakerError",
     "RateAggregatorService",
+    "ServiceFactory",
     "CurrencyManager",
 ]
