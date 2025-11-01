f61103404d79b58b0f7e81021e5a6eff21354628
Daniel Popoola <iamuchihadaniel236@gmail.com>
2025-11-01 11:15:09 +0100

    removed singleton implementation of db instance and used asynccontextmanager

--- infrastructure/persistence/database.py
+++ infrastructure/persistence/database.py
@@ -1,21 +1,39 @@
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from contextlib import asynccontextmanager
 
-from core.config import get_settings
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
 from infrastructure.persistence.models.currency import Base
-
-settings = get_settings()
 
 
 class Database:
-    def __init__(self, db_url: str = settings.DATABASE_URL):
-        self.engine = create_async_engine(db_url, echo=False)
+    def __init__(self, db_url: str):
+        self.engine = create_async_engine(db_url)
         self.session_factory = async_sessionmaker(
-            self.engine, autoflush=False, expire_on_commit=False
+            self.engine,
+            class_=AsyncSession,
+            autoflush=True,
+            expire_on_commit=False   
         )
 
-    async def create_db_and_tables(self):
+    async def create_tables(self) -> None:
         async with self.engine.begin() as conn:
             await conn.run_sync(Base.metadata.create_all)
 
+    async def drop_tables(self) -> None:
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.drop_all)
 
-database = Database()
+    async def close(self) -> None:
+        await self.engine.dispose()
+
+    @asynccontextmanager
+    async def session(self):
+        async with self.session_factory() as session:
+            try:
+                yield session
+                await session.commit()
+            except Exception:
+                await session.rollback()
+                raise
+            finally:
+                await session.close()
